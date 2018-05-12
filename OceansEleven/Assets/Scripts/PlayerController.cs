544ea3a7c89a288e0c22a98c6f61975968b59425
Luke Perry <luke.alexander.perry@gmail.com>
2018-05-12 19:11:25 +0100

    Fixed things I broke!

---
+++ OceansEleven/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int _controllerNumber = 0;
+
+    private void Start ()
+    {
+		
+	}
+
+	private void Update ()
+    {
+		if (_controllerNumber > 0)
+        {
+            PlayerControls();
+        }
+	}
+
+    void PlayerControls()
+    {
+
+    }
+
+    public void AssignController(int controller)
+    {
+        _controllerNumber = controller;
+    }
+}
