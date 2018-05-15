8a00b4b55dd719eb9d364fffd19fd40c5811408e
MTLukePerry <luke.perry@mediatonic.co.uk>
2018-05-15 14:05:52 +0100

    Use get button down rather than get button. Duh.

--- OceansEleven/Assets/Scripts/PlayerController.cs
+++ OceansEleven/Assets/Scripts/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour
             PlayerControls();
         }
 
-        if (Input.GetButton("Jump")) // TODO
+        if (Input.GetButtonDown("Jump")) // TODO
         {
             PickupItem();
         }
