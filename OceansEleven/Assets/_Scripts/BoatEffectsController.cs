7acedee554576e4eb68aabd23dae66bb750bfc38
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-05-18 00:36:02 +0100

    Setup animations to break and fix engine

---
+++ OceansEleven/Assets/_Scripts/BoatEffectsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatEffectsController : MonoBehaviour {
+
+    private Animator _anim;
+
+	// Use this for initialization
+	void Start () {
+        _anim = GetComponent<Animator>();
+	}
+	
+    public void BreakEngine()
+    {
+        _anim.SetTrigger("engineBreak");
+
+    }
+    public void FixEngine()
+    {
+        _anim.SetTrigger("engineFix");
+    }
+
+
+}
