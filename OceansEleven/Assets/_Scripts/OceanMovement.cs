3f2e2efb980b44ae38e36e3ebe0aeb413f2be3b0
Panagiotis Grigoropoulos <pgrigoropoulos@Mediatonic.local>
2018-05-17 19:40:50 +0100

    made items move in the sea

---
+++ OceansEleven/Assets/_Scripts/OceanMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanMovement : MonoBehaviour
+{
+    public Vector3 _velocity = new Vector3(-5, 0, 0);
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Boat")
+        {
+            if (collision.gameObject.GetComponent<RideTheWave>() == null)
+            {
+                var rideTheWave = collision.gameObject.AddComponent<RideTheWave>();
+                rideTheWave.velocity = _velocity;
+                collision.rigidbody.isKinematic = true;
+                collision.collider.isTrigger = true;
+            }
+        }
+    }
+}
