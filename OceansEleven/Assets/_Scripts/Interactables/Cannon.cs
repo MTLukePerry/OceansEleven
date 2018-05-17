931778aa8e5b646b97c2eb92e891d4ec54a4edb4
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-05-17 21:57:39 +0100

    Setup new cannon as old one

--- OceansEleven/Assets/_Scripts/Interactables/Cannon.cs
+++ OceansEleven/Assets/_Scripts/Interactables/Cannon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Cannon : InteractiveObject
 {
     [SerializeField] private Transform _firePosition;
