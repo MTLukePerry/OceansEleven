1b66edcccb7b4204b196777702c9a79ae7204294
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-05-17 23:30:37 +0100

    Setup wheel speed to match fuel level

--- OceansEleven/Assets/_Scripts/Animations/SimpleRotate.cs
+++ OceansEleven/Assets/_Scripts/Animations/SimpleRotate.cs
@@ -5,22 +5,32 @@ using DG.Tweening;
 
 public class SimpleRotate : MonoBehaviour {
 
-    [SerializeField] float xRotation = 360.0f;
-    [SerializeField] float yRotation = 0.0f;
-    [SerializeField] float zRotation = 0.0f;
-    [SerializeField] float rotationTime = 5.0f;
+    [SerializeField] float _xRotation = 360.0f;
+    [SerializeField] float _yRotation = 0.0f;
+    [SerializeField] float _zRotation = 0.0f;
+    public float rotationTime = 5.0f;
     [Tooltip("-1 is infinite")]
-    [SerializeField] int numberOfRotations = -1;
-    [SerializeField] bool isCounterClockwise;
+    [SerializeField] int _numberOfRotations = -1;
+    [SerializeField] bool _isCounterClockwise;
 
 	void Start () {
 
-        if (!isCounterClockwise)
-        {
-            this.transform.DOLocalRotate(new Vector3(xRotation, yRotation, zRotation), rotationTime, RotateMode.LocalAxisAdd).SetLoops(numberOfRotations).SetEase(Ease.Linear);
-        } else {
-            this.transform.DOLocalRotate(new Vector3(-xRotation, -yRotation, -zRotation), rotationTime, RotateMode.LocalAxisAdd).SetLoops(numberOfRotations).SetEase(Ease.Linear);
-        }
+        StartCoroutine(RotateTheWheel());
 	}
+
+    public  IEnumerator RotateTheWheel(){
+        while (this)
+        {
+            if (!_isCounterClockwise)
+            {
+                this.transform.DOLocalRotate(new Vector3(_xRotation, _yRotation, _zRotation), rotationTime, RotateMode.LocalAxisAdd).SetEase(Ease.Linear);
+            }
+            else
+            {
+                this.transform.DOLocalRotate(new Vector3(-_xRotation, -_yRotation, -_zRotation), rotationTime, RotateMode.LocalAxisAdd).SetEase(Ease.Linear);
+            }
+            yield return new WaitForSeconds(rotationTime);
+        }
+    }
 
 }
