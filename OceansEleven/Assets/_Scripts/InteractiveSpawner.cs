34c20f62cf3de47608eea9846cce44a773ac843f
MTLukePerry <luke.perry@mediatonic.co.uk>
2018-05-18 14:57:55 +0100

    Spawners and stuff.

---
+++ OceansEleven/Assets/_Scripts/InteractiveSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveSpawner : InteractiveObject
+{
+    [SerializeField] private GameObject _prefabToSpawn;
+
+    private Transform _spawnPosition;
+
+    private void Start()
+    {
+        _spawnPosition = transform.Find("SpawnSpot");
+    }
+
+    public override void InteractedWith(bool interacting, ObjectProperties heldObject)
+    {
+        base.InteractedWith(interacting, heldObject);
+
+        if (interacting && _prefabToSpawn != null)
+        {
+            Instantiate(_prefabToSpawn, _spawnPosition.position, Quaternion.identity);
+        }
+    }
+}
