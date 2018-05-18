76759aaff0577f7345d89ad3f480d463f0671caf
Panagiotis Grigoropoulos <pgrigoropoulos@Mediatonic.local>
2018-05-18 12:22:18 +0100

    Added the net mechanic and made physics on small objects continues

---
+++ OceansEleven/Assets/_Scripts/ActualNetScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualNetScript : MonoBehaviour
+{
+    public GameObject gameObjectToIgnore;
+
+    private void Start()
+    {
+        StartCoroutine(DestroySelf());
+    }
+
+    private IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(4);
+        Destroy(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Indestructible" &&
+            other.tag != "Boat" &&
+            other.tag != "Net" &&
+            other.gameObject != gameObjectToIgnore)
+        {
+            SingletonManager.GetInstance<NetTargetManager>().DropOnBoat(other.gameObject);
+        }
+    }
+}
---
+++ OceansEleven/Assets/_Scripts/NetTargetManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NetTargetManager : MonoBehaviour
+{
+    public float pickupTime = 2;
+
+    private List<Transform> spawnPoints = new List<Transform>();
+
+    private void Awake()
+    {
+        SingletonManager.RegisterSingleton(this);
+
+        spawnPoints = GetComponentsInChildren<Transform>().ToList();
+    }
+
+    public void DropOnBoat(GameObject obj)
+    {
+        StartCoroutine(DropOnBoatRoutine(obj));
+    }
+
+    private IEnumerator DropOnBoatRoutine(GameObject obj)
+    {
+        var rb = obj.GetComponent<Rigidbody>();
+        var col = obj.GetComponent<Collider>();
+        if (rb != null && col != null)
+        {
+            rb.isKinematic = true;
+
+            var rtw = obj.GetComponent<RideTheWave>();
+            if (rtw != null)
+            {
+                Destroy(rtw);
+            }
+
+            var randomTarget = spawnPoints[(new System.Random()).Next(0, spawnPoints.Count)];
+            var initialTransform = obj.transform;
+
+            float lerpTime = 0;
+            while (lerpTime < pickupTime)
+            {
+                lerpTime += Time.deltaTime;
+                lerpTime = Mathf.Min(pickupTime, lerpTime);
+
+                obj.transform.position = Vector3.Lerp(initialTransform.position, randomTarget.position, lerpTime / pickupTime);
+                yield return null;
+            }
+
+            rb.isKinematic = false;
+            col.isTrigger = false;
+        }
+    }
+}
