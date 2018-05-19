40b538ff525b44bd9d78fdffde2e14c4d2e61556
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-05-19 14:15:28 +0100

    Changed pickup to A, added button sprites to title and net spawn

---
+++ OceansEleven/Assets/_Scripts/Interactables/ButtonSpriteDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSpriteDisplay : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _spriteObject;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _spriteObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _spriteObject.SetActive(true);
+        }
+    }
+
+
+}
