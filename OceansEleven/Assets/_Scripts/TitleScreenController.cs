2958dc10314adfad05ad99d703034f3639ef25f7
d-ocean <daniel.ocean@mediatonic.co.uk>
2018-05-21 13:39:47 +0100

    Fixed score position and delayed initial input

--- OceansEleven/Assets/_Scripts/TitleScreenController.cs
+++ OceansEleven/Assets/_Scripts/TitleScreenController.cs
@@ -11,6 +11,7 @@ public class TitleScreenController : MonoBehaviour {
     [SerializeField] private GameObject[] _audioObjects;
     [SerializeField] private Animator _wavesAnim;
     [SerializeField] private Animator _splashAnim;
+    private bool _delayedStart = true;
 
     private bool _gameStarted=false;
 
@@ -23,32 +24,41 @@ public class TitleScreenController : MonoBehaviour {
         _startObjects.SetActive(false);
         _audioObjects[0].SetActive(true);
         _audioObjects[1].SetActive(false);
+        StartCoroutine(DelayInput());
     }
 
     void Update () {
         if (!_gameStarted)
         {
-            if (Input.anyKeyDown)
+            if (!_delayedStart)
             {
+                if (Input.anyKeyDown)
+                {
 
-                _title.SetActive(false);
-                _flag.SetActive(false);
-                _ui.SetActive(true);
-                _startObjects.SetActive(true);
-                Camera.main.GetComponent<Animator>().SetTrigger("startGame");
-                GetComponent<GameModeManager>().gameHasStarted = true;
-                //Time.timeScale = 1;
-                _wavesAnim.SetTrigger("startOcean");
-                _splashAnim.SetBool("gameHasStarted", true);
-                _audioObjects[0].SetActive(false);
-                _audioObjects[1].SetActive(true);
-                _gameStarted = true;
+                    _title.SetActive(false);
+                    _flag.SetActive(false);
+                    _ui.SetActive(true);
+                    _startObjects.SetActive(true);
+                    Camera.main.GetComponent<Animator>().SetTrigger("startGame");
+                    GetComponent<GameModeManager>().gameHasStarted = true;
+                    //Time.timeScale = 1;
+                    _wavesAnim.SetTrigger("startOcean");
+                    _splashAnim.SetBool("gameHasStarted", true);
+                    _audioObjects[0].SetActive(false);
+                    _audioObjects[1].SetActive(true);
+                    _gameStarted = true;
+                }
             }
         }
         if (Input.GetKeyUp(KeyCode.Escape)){
             Destroy(GameObject.Find("Main"));
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
+    }
+    private IEnumerator DelayInput()
+    {
+        yield return new WaitForSeconds(1.0f);
+            _delayedStart = false;
     }
 }
 
